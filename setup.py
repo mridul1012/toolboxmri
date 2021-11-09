97e48de58ee662600f857ef6c98e5308aefab2e4
Mridul Agrawal <60789145+mridul1012@users.noreply.github.com>
2021-11-09 13:14:31 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='toolbox',
-    version='0.0.4',
+    version='0.0.5',
     author='Mridul agrawal',
     author_email='mridul16011996@gmail.com',
     description='Testing installation of Package',
